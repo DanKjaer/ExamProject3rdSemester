8b8114a1f3215832d4aef173ae3fdf08c59953fb
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-11-13 10:40:33 +0100

    knappen viser nu søgebaren

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  showSearchBar = false;
   constructor() {}
+
+  toggleSearchBar(){
+    this.showSearchBar = !this.showSearchBar;
+  }
 }
