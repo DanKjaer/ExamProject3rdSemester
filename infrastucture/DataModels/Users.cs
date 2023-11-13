eefb2da3a0ba07848cd683194bf7944cb8c86944
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-11-13 10:53:52 +0100

    user

---
+++ infrastucture/DataModels/Users.cs
@@ -0,0 +1,14 @@
+namespace infrastucture.DataModels;
+
+public class Users
+{
+    public int UserId { get; set; }
+    
+    public string UserEmail { get; set; }
+    
+    public string UserName { get; set; }
+    
+    public int PhoneNumber { get; set; }
+    
+    public int UserType { get; set; }
+}
