9b6bd1766d3a19b0a072713a5895958b144694cf
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-11-14 10:56:26 +0100

    profil billede

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {CustomToolbarComponent} from "./custom-toolbar/custom-toolbar.component";
 import {AnimalBoxComponent} from "./animal-box/animal-box.component";
+import {ProfilePictureComponent} from "./profile-picture/profile-picture.component";
 
 @NgModule({
-    declarations: [AppComponent, CustomToolbarComponent, AnimalBoxComponent],
+    declarations: [AppComponent, CustomToolbarComponent, AnimalBoxComponent, ProfilePictureComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
