59b540c3bee27003ef6c0d8f171fe9fe69a4cca3
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-11-14 10:38:34 +0100

    Animal box HTML/CSS completed (i think)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {CustomToolbarComponent} from "./custom-toolbar/custom-toolbar.component";
+import {AnimalBoxComponent} from "./animal-box/animal-box.component";
 
 @NgModule({
-    declarations: [AppComponent, CustomToolbarComponent],
+    declarations: [AppComponent, CustomToolbarComponent, AnimalBoxComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
