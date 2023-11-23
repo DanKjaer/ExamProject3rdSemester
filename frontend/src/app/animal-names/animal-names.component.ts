dd380aa95dbe0b81347e9ced554caff5b2d833af
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2023-11-23 11:43:42 +0100

    jeg er gal

--- frontend/src/app/animal-names/animal-names.component.ts
+++ frontend/src/app/animal-names/animal-names.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {state} from "@angular/animations";
 import {State} from "../../state";
 import {firstValueFrom} from "rxjs";
-import {AnimalFeed} from "../../models";
+import {AnimalFeed, AnimalSpecies} from "../../models";
 import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-animal-names',
@@ -13,16 +13,21 @@ import {Router} from "@angular/router";
 })
 export class AnimalNamesComponent  implements OnInit {
 
-  constructor(public state: State, public http: HttpClient, public router: Router) { }
+  constructor(public state: State, public http: HttpClient, public router: Router, public route: ActivatedRoute) { }
+
+  speciesId?: string | null;
 
   ngOnInit() {
-    this.getAnimalFeed(<number>this.state.currentAnimalSpecies.speciesID);
+    //gets species id from route
+    this.route.paramMap.subscribe(params => {
+      this.speciesId = params.get('id')
+    })
+    this.getAnimalFeed();
   }
 
-  async getAnimalFeed(speciesId: number){
+  async getAnimalFeed(){
     try{
-      const result = await firstValueFrom(this.http.get<AnimalFeed[]>('http://localhost:5000/api/animalfeed/' + speciesId));
-      console.log(result);
+      const result = await firstValueFrom(this.http.get<AnimalFeed[]>('http://localhost:5000/api/animalfeed/' + this.speciesId));
       this.state.animalFeed = result!;
     }catch(error){
       console.error('Error fetching data:', error)
@@ -30,6 +35,8 @@ export class AnimalNamesComponent  implements OnInit {
   }
 
   goToAnimal(animalId: number) {
-    this.router.navigate(['/animals/' + animalId])
+    this.state.currentAnimal.animalID = animalId;
+    this.router.navigate(['/animals/' + animalId]);
+    console.log('click')
   }
 }
