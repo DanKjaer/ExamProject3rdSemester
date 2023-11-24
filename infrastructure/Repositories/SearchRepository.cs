7a1b022e38d9a8dd1d8892d0efa7eaea4fa124bf
Lasse Høeg <93433669+hoeeg79@users.noreply.github.com>
2023-11-24 11:39:16 +0100

    only needs testing

---
+++ infrastructure/Repositories/SearchRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using infrastructure.datamodels;
+using Npgsql;
+
+namespace infrastructure.Repositories;
+
+public class SearchRepository
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public SearchRepository(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+
+    public IEnumerable<AnimalSpeciesFeed> SearchAnimalSpecies(string searchTerm)
+    {
+        string sql = "SELECT SpeciesID, SpeciesName " +
+                     "FROM AnimalDB.AnimalSpecies " +
+                     "WHERE SpeciesName ILIKE '%' || @searchTerm || '%';";
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.Query<AnimalSpeciesFeed>(sql, new {searchTerm});
+        }
+    }
+
+    public IEnumerable<AnimalFeed>? SearchAnimals(string searchTerm)
+    {
+        string sql = "SELECT SpeciesID, SpeciesName " +
+                     "FROM AnimalDB.Animals " +
+                     "WHERE AnimalName ILIKE '%' || @searchTerm || '%';";
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.Query<AnimalFeed>(sql, new {searchTerm});
+        }
+    }
+}
