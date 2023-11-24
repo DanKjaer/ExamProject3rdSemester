40b21a228d7a6f01439fc4285fda273c0e40ba8b
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2023-11-24 11:46:09 +0100

    animal info

--- frontend/src/app/animals/animals.component.ts
+++ frontend/src/app/animals/animals.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {state} from "@angular/animations";
+import {firstValueFrom} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {State} from "../../state";
+import {ActivatedRoute} from "@angular/router";
+import {Animals, AnimalSpecies} from "../../models";
 
 @Component({
   selector: 'app-animals',
@@ -8,9 +13,26 @@ import {state} from "@angular/animations";
 })
 export class AnimalsComponent  implements OnInit {
 
-  constructor() { }
+  animalId?: string | null;
+  constructor(public http: HttpClient, public state: State, public route: ActivatedRoute) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.animalId = params.get('id')
+    })
+    this.getAnimal();
+  }
 
-  protected readonly state = state;
+  async getAnimal() {
+    const result = await firstValueFrom(this.http.get<Animals>('http://localhost:5000/api/animal/' + this.animalId));
+    console.log(result)
+    this.state.currentAnimal = result;
+    this.getSpeciesName(this.state.currentAnimal.speciesID);
+  }
+
+  async getSpeciesName(speciesId: number | undefined) {
+    const result = await firstValueFrom(this.http.get<AnimalSpecies>('http://localhost:5000/api/animalspecies/' + speciesId));
+    this.state.currentAnimalSpecies = result;
+  }
+
 }
